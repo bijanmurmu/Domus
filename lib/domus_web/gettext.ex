f98a06b4eeb1e88be70fb67e2dc349bdc13effdb
Bijan Murmu <bijanmurmu123@gmail.com>
2026-06-19 01:22:58 +0530

    Fix gettext deprecation warning

--- lib/domus_web/gettext.ex
+++ lib/domus_web/gettext.ex
@@ -2,16 +2,6 @@ defmodule DomusWeb.Gettext do
   @moduledoc """
   A module providing Internationalization with a gettext-based API.
 
-  By using [Gettext](https://hexdocs.pm/gettext),
-  your module gains a set of macros for translations, for example:
-
-      import DomusWeb.Gettext
-
-      # Simple translation
-      gettext("Here is the string to translate")
-
-      # Plural translation
-      ngettext("Here is the string to translate",
                "Here are the strings to translate",
                3)
 
